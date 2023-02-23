e67c48c7472bfc41cc867c0212abd6fe2e7f6e1d
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-02-23 23:39:01 +0200

    add next auth

--- pages/api/restricted.js
+++ pages/api/restricted.js
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "./auth/[...nextauth]"
 
-export default async (req, res) => {
+export default protectedContent = async (req, res) => {
   const session = await getServerSession(req, res, authOptions)
 
   if (session) {
