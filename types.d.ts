800063f02153307af6467ade295bef88388af7c6
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-03-09 02:02:23 +0200

    refactor: folder change

--- types.d.ts
+++
@@ -1,5 +0,0 @@
-/** Maps authored classNames to their CSS Modules -suffixed generated classNames. */
-interface Mapping { [key: string]: string; }
-declare module '*.module.css' { const mapping: Mapping; export default mapping; }
-declare module '*.module.scss' { const mapping: Mapping; export default mapping; }
-declare module '*.module.sass' { const mapping: Mapping; export default mapping; }
---
+++ types/types.d.ts
@@ -0,0 +1,4 @@
+declare module '*.module.css'
+declare module '*.module.scss'
+declare module '*.module.sass'
+declare module '*.jpg'
