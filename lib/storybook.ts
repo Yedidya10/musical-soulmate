f068f6f108eb4370e0e2123d2943a7f43e9bfecb
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-03-09 02:38:23 +0200

    ci: adding settings

---
+++ lib/storybook.ts
@@ -0,0 +1,79 @@
+export const BREAKPOINTS = {
+  GalaxyFold: {
+    name: 'GalaxyFold',
+    styles: {
+      width: '280px',
+      height: '653px',
+    },
+  },
+  320: {
+    name: '320',
+    styles: {
+      width: '320px',
+      height: '568px',
+    },
+  },
+  375: {
+    name: '375',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+  },
+  414: {
+    name: '414',
+    styles: {
+      width: '414px',
+      height: '736px',
+    },
+  },
+  390: {
+    name: '390',
+    styles: {
+      width: '390px',
+      height: '844px',
+    },
+  },
+  428: {
+    name: '428',
+    styles: {
+      width: '428px',
+      height: '926px',
+    },
+  },
+  360: {
+    name: '360',
+    styles: {
+      width: '360px',
+      height: '640px',
+    },
+  },
+  sm: {
+    name: 'sm',
+    styles: {
+      width: '600px',
+      height: '800px',
+    },
+  },
+  md: {
+    name: 'md',
+    styles: {
+      width: '900px',
+      height: '1200px',
+    },
+  },
+  lg: {
+    name: 'lg',
+    styles: {
+      width: '1200px',
+      height: '1600px',
+    },
+  },
+  xl: {
+    name: 'xl',
+    styles: {
+      width: '1536px',
+      height: '2048px',
+    },
+  },
+}
