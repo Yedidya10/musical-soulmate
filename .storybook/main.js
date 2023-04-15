c64c359664471b8282aab6fc311e521704fbd5b0
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-04-15 23:35:09 +0300

    ci: define utils resolver

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,17 @@
 module.exports = {
+  resolve: {
+    fallback: {
+      util: require.resolve("util/"),
+    },
+  },
   stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../public'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/preset-scss',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/preset-scss', "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
-}
+  docs: {
+    autodocs: true
+  }
+};
