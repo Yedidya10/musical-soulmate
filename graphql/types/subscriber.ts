bde4c2ca3dad0077265cb2824d4d0813b6219003
nauroz <alinauroze@hotmail.com>
2023-04-29 02:32:33 +0500

    create subscriber mutation

--- graphql/types/subscriber.ts
+++ graphql/types/subscriber.ts
@@ -1,11 +1,21 @@
-import prisma from '../../lib/prisma'
-import { builder } from '../builder'
+import prisma from '../../lib/prisma';
+import { builder } from '../builder';
+
+export const CreateSubscriberInput = builder.inputType('CreateSubscriberInput', {
+  fields: (t) => ({
+    email: t.string(),
+    language: t.string(),
+    country: t.string(),
+  })
+});
 
 builder.prismaObject('Subscriber', {
   name: 'Subscriber',
   fields: (t) => ({
     id: t.exposeID('id'),
     email: t.exposeString('email'),
+    country: t.exposeString('country'),
+    language: t.exposeString('language'),
     // createdAt: t.expose('createdAt', { type: 'Date' }),
   }),
 })
@@ -17,3 +27,20 @@ builder.queryField('subscribers', (t) =>
       prisma.subscriber.findMany({ ...query }),
   })
 )
+
+builder.mutationField('addSubscriber', (t) => 
+  t.prismaField({
+    type: 'Subscriber',
+    args: {
+      input: t.arg({
+        type: CreateSubscriberInput,
+        required: true
+      })
+    },
+    resolve: async (query, _parent, _args, _ctx, _info) => {
+      try {
+        return prisma.subscriber.create({ data: _args.input });
+      }catch (err) {}
+    }
+  })
+);
