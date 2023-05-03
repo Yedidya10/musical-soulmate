bd41153ba1cbefc39afcc583b1c7308664226c91
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-05-03 20:57:42 +0300

    refactor: code improvement for ts

--- graphql/types/subscriber.ts
+++ graphql/types/subscriber.ts
@@ -1,14 +1,18 @@
-import prisma from '../../lib/prisma';
-import { builder } from '../builder';
+import { Prisma } from '@prisma/client'
+import prismaClient from '@/lib/prisma'
+import { builder } from '../builder'
 
 // Input for the addSubscriber mutation
-export const CreateSubscriberInput = builder.inputType('CreateSubscriberInput', {
-  fields: (t) => ({
-    email: t.string(),
-    language: t.string(),
-    country: t.string(),
-  })
-});
+export const CreateSubscriberInput = builder.inputType(
+  'CreateSubscriberInput',
+  {
+    fields: (t) => ({
+      email: t.string({ required: true }),
+      language: t.string({ required: true }),
+      country: t.string({ required: true }),
+    }),
+  }
+)
 
 builder.prismaObject('Subscriber', {
   name: 'Subscriber',
@@ -17,7 +21,6 @@ builder.prismaObject('Subscriber', {
     email: t.exposeString('email'),
     country: t.exposeString('country'),
     language: t.exposeString('language'),
-    // createdAt: t.expose('createdAt', { type: 'Date' }),
   }),
 })
 
@@ -25,12 +28,12 @@ builder.queryField('subscribers', (t) =>
   t.prismaField({
     type: ['Subscriber'],
     resolve: (query, _parent, _args, _ctx, _info) =>
-      prisma.subscriber.findMany({ ...query }),
+    prismaClient.subscriber.findMany({ ...query }),
   })
 )
 
 // addSubscriber mutation
-builder.mutationField('addSubscriber', (t) => 
+builder.mutationField('addSubscriber', (t) =>
   t.prismaField({
     // return an object of type Subscriber
     type: 'Subscriber',
@@ -38,14 +41,17 @@ builder.mutationField('addSubscriber', (t) =>
     args: {
       input: t.arg({
         type: CreateSubscriberInput,
-        required: true
-      })
+        required: true,
+      }),
     },
+
     // resolver for the mutation. creates a subscriber in database
     resolve: async (query, _parent, _args, _ctx, _info) => {
       try {
-        return prisma.subscriber.create({ data: _args.input });
-      }catch (err) {}
-    }
+        return prismaClient.subscriber.create({ data: _args.input })
+      } catch (err: any) {
+        throw new Error(err.message)
+      }
+    },
   })
-);
+)
