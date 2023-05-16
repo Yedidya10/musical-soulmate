5951a26cb270a74b515078edb2c2402cfb38f923
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-05-16 20:15:39 +0300

    feat: new recoil atom

---
+++ recoil/atoms/musicPlayerAtom.ts
@@ -0,0 +1,41 @@
+import { atom } from 'recoil'
+
+const localStorageEffect =
+  (key: string) =>
+  ({ setSelf, onSet }: any) => {
+    if (typeof window !== 'undefined') {
+      const savedValue = localStorage.getItem(key)
+      if (savedValue != null) {
+        setSelf(JSON.parse(savedValue))
+      }
+    }
+
+    onSet((newValue: any, _: any, isReset: boolean) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue))
+    })
+  }
+
+export const musicPlayerAtom = atom({
+  key: 'musicPlayerState',
+  default: {
+    display: false,
+    track: {
+      id: '',
+      name: '',
+      artist: '',
+      album: '',
+      image: '',
+      duration: '',
+      uri: '',
+    },
+    isPlaying: false,
+    paused: false,
+    progress: 0,
+    volume: 30,
+    trackLoop: false,
+    shuffle: false,
+  },
+  effects: [localStorageEffect('musicPlayerState')],
+})
