d4643a250f30cbe8bb8dbde1f2159d36a2aa5430
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-05-22 01:19:09 +0300

    refactor: moving the MusicPlayer to PrimaryLayout

--- src/components/layouts/primary/PrimaryLayout.tsx
+++ src/components/layouts/primary/PrimaryLayout.tsx
@@ -2,12 +2,24 @@ import Head from 'next/head'
 import Footer from '../../navigation/footer/Footer'
 import Header from '../../navigation/header/Header'
 import Box from '@mui/material/Box'
+import MusicPlayer from '../../musicPlayer/MusicPlayer'
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 export interface IPrimaryLayout extends React.ComponentPropsWithoutRef<'div'> {
   headTitle: string
 }
 
 const PrimaryLayout: React.FC<IPrimaryLayout> = ({ headTitle, children }) => {
+  const [client, setClient] = useState(false)
+  const { locale } = useRouter()
+
+  useEffect(() => {
+    if (locale !== undefined) {
+      setClient(true)
+    }
+  }, [])
+
   return (
     <>
       <Head>
@@ -18,6 +30,7 @@ const PrimaryLayout: React.FC<IPrimaryLayout> = ({ headTitle, children }) => {
       </Head>
       <Header />
       <Box component={'main'}>{children}</Box>
+      {client && <MusicPlayer />}
       <Footer />
     </>
   )
