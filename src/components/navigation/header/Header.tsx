8446084f7718a9d07a73ac399deeccfe05cd8a47
Yedidya Averjel <86730512+Yedidya10@users.noreply.github.com>
2023-05-22 22:20:08 +0300

    Themes (#5)
    * feat: creating 2 themes
    
    Add ColorModeContext
    Add ColorModeProvider
    
    * feat: setting a theme in PrimaryLayout
    
    * refactor: update files path
    
    * feat: add toggleColorMode button
    
    * feat: adds secureSetItem function
    
    * refactor: moving the countryData

--- src/components/navigation/header/Header.tsx
+++ src/components/navigation/header/Header.tsx
@@ -1,10 +1,18 @@
-import Image from 'next/image'
+import Brightness4Icon from '@mui/icons-material/Brightness4'
+import Brightness7Icon from '@mui/icons-material/Brightness7'
 import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
+import { useTheme } from '@mui/material/styles'
+import Image from 'next/image'
+import React, { useContext } from 'react'
+import ColorModeContext from '../../../reactContext/ColorModeContext'
 
 import MainNav from '../mainNav/MainNav'
 import styles from './Header.module.scss'
+
 import useScrollTrigger from '@mui/material/useScrollTrigger'
 import Slide from '@mui/material/Slide'
+
 
 export interface IHeader extends React.ComponentPropsWithoutRef<'header'> {
   children?: React.ReactNode
@@ -26,15 +34,31 @@ function HideOnScroll(props: HideOnScrollProps) {
 }
 
 const Header: React.FC<IHeader> = ({ ...headerProps }) => {
+  const theme = useTheme()
+  const colorMode = useContext(ColorModeContext)
+
   return (
+
     <>
       <HideOnScroll {...headerProps}>
         <Box component={'header'} className={styles.header} {...headerProps}>
           <Box className={styles.headerContent}>
             <Image src="/logo.svg" alt="Logo" width={50} height={50} />
+                    <IconButton
+          sx={{ ml: 1 }}
+          onClick={colorMode.toggleColorMode}
+          color="inherit"
+        >
+          {theme.palette.mode === 'dark' ? (
+            <Brightness7Icon />
+          ) : (
+            <Brightness4Icon />
+          )}
+        </IconButton>
           </Box>
         </Box>
       </HideOnScroll>
+
       <MainNav />
     </>
   )
