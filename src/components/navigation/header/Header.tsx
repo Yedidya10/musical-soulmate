cd7e72f3efcb2f06e1282976750bffd33dc42b26
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-05-22 21:37:59 +0300

    feat: adds HideOnScroll element

--- src/components/navigation/header/Header.tsx
+++ src/components/navigation/header/Header.tsx
@@ -3,17 +3,40 @@ import Box from '@mui/material/Box'
 
 import MainNav from '../mainNav/MainNav'
 import styles from './Header.module.scss'
+import useScrollTrigger from '@mui/material/useScrollTrigger'
+import Slide from '@mui/material/Slide'
 
-export interface IHeader extends React.ComponentPropsWithoutRef<'header'> {}
+export interface IHeader extends React.ComponentPropsWithoutRef<'header'> {
+  children?: React.ReactNode
+}
+
+interface HideOnScrollProps {
+  children: React.ReactElement
+}
+
+function HideOnScroll(props: HideOnScrollProps) {
+  const { children } = props
+  const trigger = useScrollTrigger()
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  )
+}
 
 const Header: React.FC<IHeader> = ({ ...headerProps }) => {
   return (
-    <Box component={'header'} className={styles.header} {...headerProps}>
-      <Box className={styles.headerContent}>
-        <Image src="/logo.svg" alt="Logo" width={50} height={50} />
-      </Box>
+    <>
+      <HideOnScroll {...headerProps}>
+        <Box component={'header'} className={styles.header} {...headerProps}>
+          <Box className={styles.headerContent}>
+            <Image src="/logo.svg" alt="Logo" width={50} height={50} />
+          </Box>
+        </Box>
+      </HideOnScroll>
       <MainNav />
-    </Box>
+    </>
   )
 }
 
