120c0433886d5e2956a09da9377c6357216a5fbb
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-05-30 17:32:33 +0300

    refactor: consolidating databases config

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,14 @@
 import { Redis } from '@upstash/redis'
+import { createClient } from '@supabase/supabase-js'
 
-export const db = new Redis({
+// Upstash client for Redis. Used for real-time data storage.
+export const redisDb = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL as string,
   token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
 })
 
+// Supabase client for PostgreSQL. Used for general data storage.
+export const postgresqlDb = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+)
