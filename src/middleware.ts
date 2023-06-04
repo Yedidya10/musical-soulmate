dcc62e33aa54a017b5be64bfb466aa3cbb09267b
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-06-05 00:15:53 +0300

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,14 @@
-import { NextApiRequest } from 'next'
+import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 
-export async function middleware(req: NextApiRequest) {
+export async function middleware(req: NextRequest) {
   // Check if JWT_SECRET and DOMAIN are defined
   if (!process.env.JWT_SECRET || !process.env.DOMAIN) {
     console.error('JWT_SECRET or DOMAIN is not defined')
   }
 
   try {
-    // @ts-ignore
     const { pathname } = req.nextUrl
 
     const token = await getToken({
@@ -24,13 +23,19 @@ export async function middleware(req: NextApiRequest) {
 
     // Redirect authenticated users from /welcome to /
     if (token && pathname === '/welcome') {
-      return NextResponse.redirect(`${process.env.DOMAIN}/`)
+      return NextResponse.redirect(`${process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : process.env.DOMAIN}/`)
     }
 
     // Redirect unauthenticated users to /welcome
-    // if (!token && pathname !== '/welcome') {
-    //   return NextResponse.redirect(`${process.env.DOMAIN}/welcome`)
-    // }
+    if (!token && pathname !== '/welcome') {
+      return NextResponse.redirect(
+        `${
+          process.env.NODE_ENV !== 'production'
+            ? 'http://localhost:3000'
+            : process.env.DOMAIN
+        }/welcome`
+      )
+    }
   } catch (error) {
     console.error('Error in middleware:', error)
   }
