8db5a77bc06e6dbae9b4daadc94a89063928735a
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-06-05 17:24:12 +0300

    fix: adding matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,3 +37,16 @@ export async function middleware(req: NextRequest) {
     console.error('Error in middleware:', error)
   }
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
